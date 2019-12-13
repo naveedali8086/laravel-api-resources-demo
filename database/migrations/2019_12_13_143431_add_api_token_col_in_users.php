0789fe73ee800f6a6111332a7b6fd04d0b1f8431
Naveed Ali <naveedali8086@gmail.com>
2019-12-13 19:45:58 +0500

    created required tables and columns

---
+++ database/migrations/2019_12_13_143431_add_api_token_col_in_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddApiTokenColInUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            /* Column name 'api_token' must match the 'storage_key' defined in config/auth.php configuration file */
+            $table->string('api_token')->unique()->nullable()->default(null);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
